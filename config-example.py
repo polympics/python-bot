aaa533fc8747ef19a3a88b7af81fc42a4c9fe55f
Jasper Harrison <legorooj@protonmail.com>
2021-06-19 15:53:59 +1000

    First version of the bot

---
+++ config-example.py
@@ -0,0 +1,19 @@
+import os
+
+secret = os.urandom(16).hex()
+
+# Discord Bot Token
+discord_token = ''
+
+# Api token and username for the Polympics API
+api_token = ''
+api_user = ''
+
+# Polympics API URL
+base_url = ''
+
+# Callback URL which receives POSTs
+callback_url = ''
+
+# Port on which to serve the app
+port = 13130
---
+++ main.py
@@ -0,0 +1,54 @@
+import asyncio
+import sys
+
+import discord
+import polympics
+from aiohttp import web
+
+import config
+
+client = discord.Client()
+server = web.Application()
+polympics_client: [polympics.AppClient, None] = None
+
+
+async def callback(request: web.Request):
+    print(request.headers)
+    print(await request.json())
+    
+    return web.Response(status=200)
+
+
+@client.event
+async def on_ready():
+    global polympics_client
+    
+    polympics_client = polympics.AppClient(
+        polympics.Credentials(config.api_user, config.api_token),
+        base_url=config.base_url
+    )
+    
+    await polympics_client.create_callback(
+        polympics.EventType.ACCOUNT_TEAM_UPDATE,
+        config.callback_url,
+        config.secret
+    )
+    
+    # Add the routes for the server
+    server.add_routes(
+        [web.post("/", callback)]
+    )
+    # Create an AppRunner
+    runner = web.AppRunner(server)
+    # Set it up
+    await runner.setup()
+    # Create a TCPSite object which actually serves the callback
+    site = web.TCPSite(runner, 'localhost', config.port)
+    # Start the site
+    await site.start()
+
+
+if __name__ == '__main__':
+    print(config.secret)
+    
+    client.run(config.discord_token)
